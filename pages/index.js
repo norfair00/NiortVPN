a3721c78149aefe95a41469964519c40f7b90802
Nicolas Favre <nicolas.f@jaipasd.email>
2022-07-28 08:03:10 +0200

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,19 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>VPN Best Landingpage </title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Le seul vrai VPN • NiortVPN</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png?v=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png?v=1" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png?v=1" />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest?v=1" />
+        <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg?v=1" color="#5092ff" />
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico?v=1" />
+        <meta name="apple-mobile-web-app-title" content="NiortVPN" />
+        <meta name="application-name" content="NiortVPN" />
+        <meta name="msapplication-TileColor" content="#5092ff" />
+        <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml?v=1" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Layout>
         <Hero />
